0b1a32556dc117c193321f8637cd8dba754c376a
GunanithiMurugesan <138852458+GunanithiMurugesan@users.noreply.github.com>
2024-04-08 15:09:26 +0530

    Home, Reports screen and page not found restrictions are added
    Home, Reports screen and page not found screen restrictions are added

--- Angular17_DemoProject/src/app/app.component.ts
+++ Angular17_DemoProject/src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SampleRenderingPageComponent } from './core-components/sample-rendering-page/sample-rendering-page.component';
 import { HeaderComponent } from './basic-components/header/header.component';
 import { PageUnderDevComponent } from './basic-components/page-under-dev/page-under-dev.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SampleRenderingPageComponent,HeaderComponent],
+  imports: [RouterOutlet, SampleRenderingPageComponent, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular17_DemoProject';
+  includePaths: string[] = ['', 'home', 'reports'];
+  hideNav = false;
+
+  constructor(private router: Router) { }
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const endpoint = this.getEndpointFromUrl(event.urlAfterRedirects);
+        this.hideNav = this.includePaths.includes(endpoint);
+      }
+    });
+  }
+  private getEndpointFromUrl(url: string): string {
+    const segments = url.split('/');
+    return segments[segments.length - 1];
+  }
 }
--- Angular17_DemoProject/src/app/app.routes.ts
+++ Angular17_DemoProject/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core-components/home/home.component';
 import { ReportsComponent } from './core-components/reports/reports.component';
+import { PageUnderDevComponent } from './basic-components/page-under-dev/page-under-dev.component';
+import { PageNotFoundComponent } from './basic-components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,10 +12,14 @@ export const routes: Routes = [
     },
     {
         path: 'home',
-        component:HomeComponent
+        component: HomeComponent
     },
     {
-        path:'reports',
-        component:ReportsComponent
+        path: 'reports',
+        component: ReportsComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
     }
 ];
