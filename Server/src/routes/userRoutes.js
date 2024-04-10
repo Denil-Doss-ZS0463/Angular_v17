3dec436f36c110321b8d39191d4e2b743aa62ece
SaranyaSekar16 <saranya.sekar@zucisystems.com>
2024-04-10 13:16:39 +0530

    added endpoints for getuserbyid

--- Server/src/routes/userRoutes.js
+++ Server/src/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const UserController = require("../controllers/UserController");
+const UserController = require("../controllers/UserController")
 
+router.get('/getUserById/:id', UserController.getUserById);
 router.get("/getAllUsers", UserController.getAllUsers);
+router.post('/addUser', UserController.addUser);
 router.post("/login", UserController.login);
+
 module.exports = router;
