c3ded511e969f5bcbe769e182c6f83312d2ada61
Denil0805 <denil.doss@zucisystems.com>
2024-04-22 13:20:05 +0530

    Change Password Validation has been added to the frontend
    Change Password Validation has been added to the frontend

--- Angular17_DemoProject/src/app/common-libraies/common-input-box/common-input-box.component.ts
+++ Angular17_DemoProject/src/app/common-libraies/common-input-box/common-input-box.component.ts
@@ -1,7 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormsModule } from '@angular/forms';
-
+import { StringContants } from '../../../assets/Constants/stringConstant';
 @Component({
   selector: 'app-common-input-box',
   standalone: true,
@@ -21,8 +21,22 @@ export class CommonInputBoxComponent {
   @Input() required!: boolean;
   @Input() disabled: boolean = false;
 
+  toggleEye: boolean = false;
+  passwordLengthError:string = StringContants.changePassword.passwordLengthErrorText;
+  passwordNotMatchError:string = StringContants.changePassword.passwordNotMatchErrorText;
+  passwordRegexError:string = StringContants.changePassword.passwordRegexErrorText;
+  
   showErrors(): boolean | null {
     const control = this.parentForm.get(this.controlName);
     return control && control.invalid && control.touched || this.invalidForm;
+  }
+
+  toggleEyeIcon() {
+    this.toggleEye = !this.toggleEye;
+    if (this.toggleEye) {
+      this.fieldType = 'text';
+    } else {
+      this.fieldType = 'password';
+    }
   }
 }
